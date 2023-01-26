6fd7885526f9be14a75b9ee9e56136582d8cbafa
Kleber Henrique <yuk9.gabriel@gmail.com>
2023-01-26 17:59:25 -0300

    Listagem de grupos

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,24 @@
-import { Header } from '@components/Header';
-import * as S from './styles'; //importando tudo de dentro do styles e colocando S como nome
+import { GroupCard } from "@components/GroupCard";
+import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
+import { useState } from "react";
+import { FlatList } from "react-native";
+import * as S from "./styles"; //importando tudo de dentro do styles e colocando S como nome
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(["", ""]);
   return (
     <S.Container>
       <Header />
+
+      <Highlight title="Turmas" subtitle="jogue com a sua turma" />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+      />
+
     </S.Container>
   );
-};
+}
